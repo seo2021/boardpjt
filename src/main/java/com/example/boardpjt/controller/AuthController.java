634173f05623d0251e313d2430826d92cd13f17b
notlikelion <notlikelion@gmail.com>
2025-09-05 14:08:13 +0900

    푸시

--- src/main/java/com/example/boardpjt/controller/AuthController.java
+++ src/main/java/com/example/boardpjt/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller // view -> template -> html(thymeleaf)
 @RequiredArgsConstructor // 생성자 주입
@@ -23,10 +24,19 @@ public class AuthController {
     // 해당 처리를 Service로 전달해주는...
     @PostMapping("/register") // POST
     public String register(@RequestParam String username,
-                           @RequestParam String password) {
+                           @RequestParam String password,
+                           RedirectAttributes redirectAttributes) {
         // @Valid -> 유효성 검증
-        userAccountService.register(username, password);
-        return "redirect:/";
+        try {
+            userAccountService.register(username, password);
+            return "redirect:/";
+        } catch (IllegalArgumentException e) {
+            // 중복사용자
+//            redirectAttributes.addAttribute("error", e.getMessage()); // Model이 받아서 쓸 수 있게 RequestParam으로 주는 것
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+            // 자동으로 model에 넣어줘서 request attribute로 꺼내 쓸 수 있음
+            return "redirect:/auth/register";
+        }
 //        return "redirect:/auth/login"; // login은 없으니까 403?
     }
 }
